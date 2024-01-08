62f1d8d5e0ca29c8db66406f3133589aa276b470
hdaojin <hdaojin@hotmail.com>
2024-01-08 08:47:47 +0800

    增加ansible-env.sh

---
+++ system-initialization/ansible-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Name: ansible-env.sh
+# Description: Initialization for ansible
+# Author: Huangdaojin
+# Date: 2024.1.7
+# update: 2024.1.7
+# Version: 0.0.1
+# Usage: bash ansible-env.sh
+# Applicable: Debian 12
+
+
+# Install necessary packages for ansible running
+
+apt update -y
+apt upgrade -y 
+apt install -y python3 ansible sshpass
+
+# Generate ssh key
+ssh-keygen  -t rsa -b 2048 -f ~/.ssh/id_rsa -N "" 
